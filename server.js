b4e07c148abf2bb104cc5d7e744af62fccf17f4a
Yamin Moulay <yaminmoulay12@gmail.com>
2024-09-05 15:20:51 +0200

    small ajustments to server.js

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
 require("dotenv").config();
 
-mongoose.connect(process.env.DATABASE_URL , {useNewUrlParsel: true})
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error))
 db.once("open", () => console.log("Connected to Database"))
 
 
 app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
 
 const lineupRouter = require("./routes/lineup"); 
 app.use("/lineup", lineupRouter)
-"localhosrt:3000/lineup"
+
 
 
 app.listen(3000, () => console.log("server started on port 3000"));
