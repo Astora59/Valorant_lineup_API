7ef3d7aa550d2b351db3b073e78e1efa38f4c658
Yamin Moulay <yaminmoulay12@gmail.com>
2024-10-29 22:31:47 +0100

    re-defined console.log message upon server starting

--- server.js
+++ server.js
@@ -43,4 +43,4 @@ const errorHandler = error => {
   const port = process.env.PORT || 3000;
   
 
-app.listen(port, () => console.log("server started on port 3000"));
+app.listen(port, () => console.log("server started on port ${port}"));
